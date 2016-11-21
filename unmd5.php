3f48b53a493e896b44af100eeacae8ff87f7866c
João Paulo Polles <joaopaulopolles@gmail.com>
2016-11-21 02:41:53 -0200

    Initial Commit

---
+++ unmd5.php
@@ -0,0 +1,8 @@
+<?php
+
+$hash = "37b51d194a7513e45b56f6524f2d51f2";  // I know this is 'bar'
+
+$h0 = substr($hash, 0, 7);
+$h1 = substr($hash, 8, 15);
+$h2 = substr($hash, 16, 23);
+$h3 = substr($hash, 24, 31);
